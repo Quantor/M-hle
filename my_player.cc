db2db3ec7e3cbdc8607dcdf2a67a0d6a0a67e9a4
„Oliver <jensch.oliver@web.de>
2017-03-08 17:14:33 +0100

    lol.2

--- my_player.cc
+++ my_player.cc
@@ -2,17 +2,32 @@
 #include <stdio.h>
 #include <iostream>
 
-class Connections 
+class Connections
 {
-	public:
-		char Knoten1;
-		char Knoten2;
+public:
+	char Knoten1;
+	char Knoten2;
+};
+
+/* convert player number to board symbols */
+static char num2sym(char input)
+{
+	switch (input) {
+	case '0':
+		return 'X';
+	case '1':
+		return 'O';
+	case '.':
+		return ' ';
+	default:
+		return input;
+	}
 }
 
 // Search for a specific charakter on the board
 int board_search(char Zeichen, char board[], int number, int trys)
 {
-	for (int i = 0+trys; i < number; i++)
+	for (int i = 0 + trys; i < number; i++)
 	{
 		if (board[i] == Zeichen)
 		{
@@ -23,22 +38,28 @@ int board_search(char Zeichen, char board[], int number, int trys)
 }
 
 //Search for connection between 'A' + i and a piece on board
-char connect_search(int i, char board[], int number, Connections edge, int number)
+char connect_search(int i, char board[], int number,
+	Connections edge[], int n, unsigned current_player)
 {
-	for (int k = 0; k < 64; k++)
+	for (int k = 0; k < n; k++)
 	{
 		int q, p;
 		q = edge[k].Knoten1 - 'A';
 		p = edge[k].Knoten2 - 'A';
-		if (edge[k].Knoten2 == 'A' + i && board[q] == '.')
+		if (edge[k].Knoten2 == 'A' + i &&
+			board[q] == 'X')
 		{
+			std::cout << k << std::endl;
 			return edge[k].Knoten1;
 		}
-		if (edge[k].Knoten1 == 'A' + i && board[p]=='.')
+		if (edge[k].Knoten1 == 'A' + i &&
+			board[p] == 'X')
 		{
+			std::cout << k << std::endl;
 			return edge[k].Knoten2;
 		}
 	}
+	return 0;
 }
 
 
@@ -63,26 +84,26 @@ int main(void)
 		char board[24];
 		char piece_move = ' ', piece_put = ' ', piece_kill = ' ';
 		char newline;
-		Connections edge[64];
+		Connections edge[32];
 		int matches;
 		// initialize edges with nodes
-		edge[0].Knoten1 = 'A'; edge[1].Knoten1 = 'A'; edge[2].Knoten1 = 'B'; edge[3].Knoten1 = 'B'; 
+		edge[0].Knoten1 = 'A'; edge[1].Knoten1 = 'A'; edge[2].Knoten1 = 'B'; edge[3].Knoten1 = 'B';
 		edge[0].Knoten2 = 'B'; edge[1].Knoten2 = 'J'; edge[2].Knoten2 = 'C'; edge[3].Knoten2 = 'E';
 
-		edge[4].Knoten1 = 'C'; edge[5].Knoten1 = 'D'; edge[6].Knoten1 = 'D'; edge[7].Knoten1 = 'E'; 
-		edge[4].Knoten2 = 'O'; edge[5].Knoten2 = 'E'; edge[6].Knoten2 = 'K'; edge[7].Knoten2 = 'F'; 
+		edge[4].Knoten1 = 'C'; edge[5].Knoten1 = 'D'; edge[6].Knoten1 = 'D'; edge[7].Knoten1 = 'E';
+		edge[4].Knoten2 = 'O'; edge[5].Knoten2 = 'E'; edge[6].Knoten2 = 'K'; edge[7].Knoten2 = 'F';
 
-		edge[8].Knoten1 = 'E'; edge[9].Knoten1 = 'F'; edge[10].Knoten1 = 'G'; edge[11].Knoten1 = 'G'; 
+		edge[8].Knoten1 = 'E'; edge[9].Knoten1 = 'F'; edge[10].Knoten1 = 'G'; edge[11].Knoten1 = 'G';
 		edge[8].Knoten2 = 'H'; edge[9].Knoten2 = 'N'; edge[10].Knoten2 = 'H'; edge[11].Knoten2 = 'L';
 
-		edge[12].Knoten1 = 'H'; edge[13].Knoten1 = 'I'; edge[14].Knoten1 = 'J'; edge[15].Knoten1 = 'J'; 
+		edge[12].Knoten1 = 'H'; edge[13].Knoten1 = 'I'; edge[14].Knoten1 = 'J'; edge[15].Knoten1 = 'J';
 		edge[12].Knoten2 = 'I'; edge[13].Knoten2 = 'M'; edge[14].Knoten2 = 'K'; edge[15].Knoten2 = 'V';
-		
-		edge[16].Knoten1 = 'K'; edge[17].Knoten1 = 'K'; edge[18].Knoten1 = 'L'; edge[19].Knoten1 = 'M'; 
-		edge[16].Knoten2 = 'L'; edge[17].Knoten2 = 'S'; edge[18].Knoten2 = 'P'; edge[19].Knoten2 = 'N'; 
 
-		edge[20].Knoten1 = 'M'; edge[21].Knoten1 = 'N'; edge[22].Knoten1 = 'N'; edge[23].Knoten1 = 'O'; 
-		edge[20].Knoten2 = 'R'; edge[21].Knoten2 = 'O'; edge[22].Knoten2 = 'U'; edge[23].Knoten2 = 'X'; 
+		edge[16].Knoten1 = 'K'; edge[17].Knoten1 = 'K'; edge[18].Knoten1 = 'L'; edge[19].Knoten1 = 'M';
+		edge[16].Knoten2 = 'L'; edge[17].Knoten2 = 'S'; edge[18].Knoten2 = 'P'; edge[19].Knoten2 = 'N';
+
+		edge[20].Knoten1 = 'M'; edge[21].Knoten1 = 'N'; edge[22].Knoten1 = 'N'; edge[23].Knoten1 = 'O';
+		edge[20].Knoten2 = 'R'; edge[21].Knoten2 = 'O'; edge[22].Knoten2 = 'U'; edge[23].Knoten2 = 'X';
 
 		edge[24].Knoten1 = 'P'; edge[25].Knoten1 = 'Q'; edge[26].Knoten1 = 'Q'; edge[27].Knoten1 = 'S';
 		edge[24].Knoten2 = 'Q'; edge[25].Knoten2 = 'R'; edge[26].Knoten2 = 'T'; edge[27].Knoten2 = 'T';
@@ -123,14 +144,15 @@ int main(void)
 		}
 
 		//Phase 2
-		if(unplaced_pieces[current_player]==0)
+		if (unplaced_pieces[current_player] == 0)
 		{
-			for (int trys=0; piece_move=' '; trys++)
+			for (int trys = 0; piece_move == ' '; trys++)
 			{
 				int i = board_search('.', board, 24, trys);
-				if (connect_search(i, board, 24, edge, 64))
+				std::cout << i << std::endl;
+				if (connect_search(i, board, 24, edge, 64, current_player))
 				{
-					piece_move = connect_search(i, board, 24, edge, 64);
+					piece_move = connect_search(i, board, 24, edge, 64, current_player);
 					piece_put = 'A' + i;
 				}
 			}
@@ -138,22 +160,22 @@ int main(void)
 
 
 
-			/* Output our move as three characters, followed by a newline.
-			* The first character specifies the piece to move or a space if an
-			* unplaced piece is used.
-			* The second character specifies the place to put the piece.
-			* The third character specifies an opponent's piece to remove, or a
-			* space if this is not appropriate. */
-			fprintf(move_output, "%c%c%c\n", piece_move, piece_put, piece_kill);
+		/* Output our move as three characters, followed by a newline.
+		* The first character specifies the piece to move or a space if an
+		* unplaced piece is used.
+		* The second character specifies the place to put the piece.
+		* The third character specifies an opponent's piece to remove, or a
+		* space if this is not appropriate. */
+		fprintf(move_output, "%c%c%c\n", piece_move, piece_put, piece_kill);
 
-			/* Flush the buffer so the MCP actually receives our move. */
-			fflush(move_output);
-		}
-
-		/* cleanup */
-		fclose(state_input);
-		fclose(move_output);
-
-		/* in all cases we get here, an error happened */
-		return 1;
+		/* Flush the buffer so the MCP actually receives our move. */
+		fflush(move_output);
 	}
+
+	/* cleanup */
+	fclose(state_input);
+	fclose(move_output);
+
+	/* in all cases we get here, an error happened */
+	return 1;
+}
